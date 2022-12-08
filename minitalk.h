0568ac11e91a4ddf11f884cddf03c971c84dea33
Yoshifumi-Kitagawa-2 <kitapo3110@gmail.com>
2022-12-08 18:04:21 +0900

    commit

--- minitalk.h
+++ minitalk.h
@@ -6,7 +6,16 @@
 /*   By: yokitaga <yokitaga@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/07 19:13:11 by yokitaga          #+#    #+#             */
-/*   Updated: 2022/12/07 19:13:12 by yokitaga         ###   ########.fr       */
+/*   Updated: 2022/12/08 15:32:05 by yokitaga         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#ifndef MINITALK_H
+# define MINITALK_H
+
+# include "libft.h"
+# include <signal.h>
+# include <unistd.h>
+# include <stdlib.h>
+
+#endif
--- server.c
+++ server.c
@@ -6,13 +6,41 @@
 /*   By: yokitaga <yokitaga@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/07 19:12:45 by yokitaga          #+#    #+#             */
-/*   Updated: 2022/12/07 19:44:50 by yokitaga         ###   ########.fr       */
+/*   Updated: 2022/12/08 18:04:13 by yokitaga         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-int main()
+#include "minitalk.h"
+
+
+void signal_handler(int signal, siginfo_t *info, void *context)
 {
-    while(1);
-    return 0;
+    static int pid = 0;
+
+    (void)context;
+    if (info->si_pid)
+        pid = info->si_pid;
+    
+    
 }
 
+int main(void)
+{
+    struct sigaction    sa;
+    sa.sa_handler = 0;
+    sa.sa_flags = SA_SIGINFO;
+    sa.sa_sigaction = signal_handler;
+    sigemptyset(&sa.sa_mask);
+    sigaddset(&sa.sa_mask, SIGUSR1);
+    sigaddset(&sa.sa_mask, SIGUSR2);
+    if (sigaction(SIGUSR1, &sa, NULL) == -1 || sigaction(SIGUSR2, &sa, NULL) == -1)
+    {
+        ft_putstr_fd("server: unexpected error.\n", 1);
+        return (0);
+    }
+    ft_putstr_fd("PID: ",1);
+    ft_putnbr_fd(getpid(), 1);
+    ft_putstr_fd("\n",1);
+    while(1)
+        pause();
+}

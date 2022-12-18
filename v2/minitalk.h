209b120ceba8b999a3fa48a86c7b5007193da08e
Yoshifumi-Kitagawa-2 <kitapo3110@gmail.com>
2022-12-18 17:53:37 +0900

    commit

--- v2/minitalk.h
+++ v2/minitalk.h
@@ -5,33 +5,27 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: yokitaga <yokitaga@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/12/12 13:41:29 by yokitaga          #+#    #+#             */
-/*   Updated: 2022/12/18 16:56:01 by yokitaga         ###   ########.fr       */
+/*   Created: 2022/12/18 15:56:08 by yokitaga          #+#    #+#             */
+/*   Updated: 2022/12/18 17:52:48 by yokitaga         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
 
 #ifndef MINITALK_H
 # define MINITALK_H
 
 # include <signal.h>
 # include <stdlib.h>
-# include <unistd.h>
+# include <unistd.h>>
 # include <sys/types.h>
 # include <stdbool.h>
 # include "libft/libft.h"
 
-# define CHAR 0
-# define CNT  1
 # define ZEROBIT SIGUSR1
 # define ONEBIT SIGUSR2
 # define ACKSIG SIGUSR1
 # define CMPSIG SIGUSR2
 # define EOT  4
-
-typedef struct s_signal
-{
-    size_t nbr;
-    int cnt;
-}t_signal;
+# define MAX_SIZE 1000
 
 #endif
---
+++ v2/server.c
@@ -0,0 +1,89 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   server.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: yokitaga <yokitaga@student.42tokyo.jp>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/12/18 15:56:11 by yokitaga          #+#    #+#             */
+/*   Updated: 2022/12/18 17:49:06 by yokitaga         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+# include "minitalk.h"
+#include <stdio.h>
+
+//信号ハンドラー内での変数アクセスを原子的に行うことができるという点が特徴。原子的アクセスとは、複数のスレッドから同時にアクセスされることを防ぐことを意味する。
+volatile sig_atomic_t g_atomic_data = 0;
+
+void ft_put_error_exit()
+{
+    ft_putstr_fd("Error Occured\n", 1);
+    eixt(1);
+}
+
+static int nbr_to_str(uint8_t nbr)
+{
+    static  char    *str[MAX_SIZE];
+    static  size_t  i = 0;
+
+    str[i] = nbr;
+    if (nbr == EOT)
+    {
+        str[i] = '\0';
+        i = 0;
+        ft_putstr_fd(str,1);
+        return (EOT);
+    }
+    else if (i == MAX_SIZE -1)
+    {
+        str[i] = '\0';
+        i = 0;
+         ft_putstr_fd(str,1);
+    }
+    else
+        i++;
+    return(0);
+}
+
+static void signal_handler(int signal, siginfo_t *info, void *context)
+{
+    static uint8_t  nbr = 0;
+    static uint8_t  cnt = 0;
+    (void)context;
+    const pid_t pid = info->si_pid;
+    if (signal == ONEBIT)
+        nbr = nbr << 1 | 1;
+    else if (signal == ZEROBIT)
+        nbr = nbr << 1;
+    cnt++;
+    if (cnt == 8)
+    {
+        cnt = 0;
+        if (nbr_to_str(nbr) == EOT)
+        {
+            kill(pid, CMPSIG);
+            return ;
+        }
+    }
+    kill(pid, ACKSIG);
+}
+
+int main(void)
+{
+    struct sigaction sa; //sigactio構造体を宣言。
+    sigemptyset(&sa.sa_mask); //sa_maskを空に。
+    //sa_maskにSIGUSR1/2を追加
+    if (sigaddset(&sa.sa_mask, SIGUSR1) == -1 || sigaddset(&sa.sa_mask, SIGUSR2) == -1);
+        ft_put_error_exit();
+    sa.sa_flags = SA_SIGINFO;
+    sa.sa_sigaction = &signal_handler;
+    //sigaction関数を使用することで、SIGINT信号を受け取ったときにsigint_handler関数が実行されるようになる
+    if (sigaction(SIGUSR1, &sa, NULL) == -1 || sigaction(SIGUSR2, &sa, NULL) == -1)
+        ft_put_error_exit();
+    ft_putstr_fd("PID:",1);
+    ft_putnbr_fd(getpid(), 1);
+    while(1)
+        pause();
+    return(0);
+}
